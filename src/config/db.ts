d91418e03b2d39874bbcc6283b6ad25638de6d7a
lery Castillo <yrelnhoj1822@gmail.com>
2024-04-26 19:13:47 +0800

    updated backend

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,13 @@
 const mysql = require('mysql')
+const dotenv = require('dotenv');
+dotenv.config();
+//SA .ENV PEDE KA GUMAWA NETO EXAMPLE  HOST:localhost or pede rin dirrect na kagaya ng naka OR jan na ||  'localhost' 
+
 const connection = mysql.createConnection({
-  host: 'localhost', 
-  user: 'root', //username
-  password: '111111',//password ng sql mo pag wala wagmona lagyan
-  database: 'tube_tv',//database name mo
+  host:process.env.HOST ||'localhost' , //LOCALHOST LNG TO
+  user: process.env.USER || 'root',//username ng mysql mo
+  password: process.env.PASSWORD || 'password ng mysql mo',//password ng msysql mo pag wala wag mona lagyan
+  database: process.env.DATABASE || 'database name mo',
 })
 
 
